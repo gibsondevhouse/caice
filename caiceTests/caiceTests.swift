4335afc7c6873307b66f2d696639a54d889429a4
Chris Gibson <gibsondevhouse@gmail.com>
2026-04-05 18:10:46 -0400

    Initial Commit

---
+++ caiceTests/caiceTests.swift
@@ -0,0 +1,16 @@
+//
+//  caiceTests.swift
+//  caiceTests
+//
+//  Created by Christopher Gibson on 4/5/26.
+//
+
+import Testing
+
+struct caiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
